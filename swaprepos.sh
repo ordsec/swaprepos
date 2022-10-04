8c5345c35b7414e501ec8882b89473b5c13c8a63
David Ordovskiy <david.ordovsky@gmail.com>
2022-10-04 19:23:53 -0400

    add test version and live version

--- swaprepos.sh
+++ swaprepos.sh
@@ -2,18 +2,18 @@
 
 swapToUbuntu () {
     echo "Changing apt repos to Ubuntu..."
-    mv ./test-files/sources.list ./test-files/sources.list.bak
-    # find /var/lib/apt/lists -type f -exec rm {} \;
-    wget https://gist.githubusercontent.com/ishad0w/788555191c7037e249a439542c53e170/raw/3822ba49241e6fd851ca1c1cbcc4d7e87382f484/sources.list -O ./test-files/sources.list
-    # apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32
-    # apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 871920D1991BC93C
+    mv /etc/apt/sources.list /etc/apt/sources.list.bak
+    find /var/lib/apt/lists -type f -exec rm {} \;
+    wget https://gist.githubusercontent.com/ishad0w/788555191c7037e249a439542c53e170/raw/3822ba49241e6fd851ca1c1cbcc4d7e87382f484/sources.list -O /etc/apt/sources.list
+    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3B4FE6ACC0B21F32
+    apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 871920D1991BC93C
 }
 
 swapToKali () {
     echo "Changing apt repos to Kali's defaults..."
-    # find /var/lib/apt/lists -type f -exec rm {} \;
-    mv ./test-files/sources.list ./test-files/ubuntu-sources.list
-    mv ./test-files/sources.list.bak ./test-files/sources.list
+    find /var/lib/apt/lists -type f -exec rm {} \;
+    mv /etc/apt/sources.list /etc/apt/ubuntu-sources.list
+    mv /etc/apt/sources.list.bak /etc/apt/sources.list
 }
 
 if [ `whoami` != root ]; then
@@ -28,7 +28,14 @@ if [ "$1" = -u ] || [ "$1" = --ubuntu ]; then
 elif [ "$1" = -k ] || [ "$1" = --kali ]; then
     swapToKali
 else
-    echo Usage: add later
+    echo "Usage:"
+    echo "-u or --ubuntu to change to ubuntu apt repos"
+    echo "-k or --kali to change back to default kali repos"
+    exit
 fi
 
-# apt-get update
+apt-get update
+
+ls /etc/apt | grep sources
+
+echo "Completed successfully!"
